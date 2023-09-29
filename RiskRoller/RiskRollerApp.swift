d2fab633cf3c01cc031a654c9381ea79562da113
James-Nunn <james.nunn@outlook.com.au>
2023-09-29 14:56:10 +1000

    Initial Commit

---
+++ RiskRoller/RiskRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RiskRollerApp.swift
+//  RiskRoller
+//
+//  Created by James Nunn on 29/9/2023.
+//
+
+import SwiftUI
+
+@main
+struct RiskRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
